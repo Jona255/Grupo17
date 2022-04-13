9d8db1e5ec44cb1dee8ec42e1e3ad5dbeb989b45
Jonatan Garcia <sacha.255555@gmail.com>
2022-04-13 19:17:23 -0300

    Agregados Puntos 9 y 10

---
+++ TP2_Grupo17/src/main/java/fi/unju/edu/ar/ejercicio9/Principal.java
@@ -0,0 +1,13 @@
+package fi.unju.edu.ar.ejercicio9;
+
+public class Principal {
+
+	public static void main(String[] args) {
+
+		for (int i = 320; i >= 160; i-=20) {
+			System.out.println(i);			
+		}
+		
+	}
+
+}
