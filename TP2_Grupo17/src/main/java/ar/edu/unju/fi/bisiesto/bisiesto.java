eb9bdd6ee29b245e8751852fa8f128c29c807c50
KuruKLG <kuru_gutierrez@hotmail.com>
2022-04-13 20:43:15 -0300

    Actualización puntos 2 y 3

---
+++ TP2_Grupo17/src/main/java/ar/edu/unju/fi/bisiesto/bisiesto.java
@@ -0,0 +1,30 @@
+package ar.edu.unju.fi.bisiesto;
+
+import java.util.Scanner;
+
+public class bisiesto {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Ingrese un anio: ");
+		
+		Scanner sc = new Scanner(System.in);
+		int anio = sc.nextInt();
+		
+		if (((anio % 4) == 0) && ((anio % 100) != 0))
+		{
+			System.out.println("El anio "+anio+" es anio bisiesto.");
+		} else
+		{
+			if ((anio % 400) == 0)
+			{
+				System.out.println("El anio "+anio+" es anio bisiesto.");
+			} else
+			{
+				System.out.println("El anio "+anio+" no es anio bisiesto.");
+			}
+		}
+		
+	}
+	
+}
---
+++ TP2_Grupo17/src/main/java/ar/edu/unju/fi/numeropar/NumeroPar.java
@@ -0,0 +1,29 @@
+package ar.edu.unju.fi.numeropar;
+
+import java.util.Scanner;
+
+public class NumeroPar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for (int i=1; i <= 5; i++) {
+			
+			System.out.println("Ingrese un número: ");
+			
+			Scanner sc = new Scanner(System.in);
+			int numero = sc.nextInt();
+			
+			if ((numero % 2) == 0)
+			{
+				System.out.println("El número ingresado es par.");
+			} else
+			{
+				System.out.println("El número no es par.");
+			}
+			
+		}
+		
+	}
+
+}
