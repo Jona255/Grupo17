a28f96bc0e2914d9841dd593e1269231c9ed2ba3
MCelesteFicoseco <celeste.ficoseco@gmail.com>
2022-04-14 19:11:03 -0300

    Punto 11 terminado

---
+++ TP2_Grupo17/src/main/java/ar/edu/unju/fi/ejercicio11/Ejercicio11.java
@@ -0,0 +1,16 @@
+package ar.edu.unju.fi.ejercicio11;
+
+public class Ejercicio11 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int inicio = 320, fin = 160;
+		
+		do {
+			 System.out.println(inicio);
+			 inicio = inicio - 20;
+			
+		}while(inicio >= fin);
+	}
+
+}
