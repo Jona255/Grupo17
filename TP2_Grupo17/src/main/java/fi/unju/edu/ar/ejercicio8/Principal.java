52d83ac4b71793e2fab55cbd5744fdd82e31c5d0
User <User@DESKTOP-85PBKTA>
2022-04-14 21:18:21 -0300

    Agregue ejercicio8, mostrar los multiplos de 4 hasta 100 usando do-while

---
+++ TP2_Grupo17/src/main/java/fi/unju/edu/ar/ejercicio8/Principal.java
@@ -0,0 +1,19 @@
+package fi.unju.edu.ar.ejercicio8;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// codigo que calcula los multiplos de cuatro
+		int multiplo = 4; // se inicializa variable a determinar si guarda multiplo de cuatro
+
+		do {
+			if (multiplo % 4 == 0) {// se calcula el resto entre un posible multiplo y cuatro
+				System.out.println(multiplo); // si multiplo, se muestra por pantalla
+			}
+			multiplo++; // incremento en uno la variable a determinar
+		} while (multiplo <= 100); // mientras el valor a determinar sea menor igual a 100
+		// se sigue calculando
+
+	}
+
+}
