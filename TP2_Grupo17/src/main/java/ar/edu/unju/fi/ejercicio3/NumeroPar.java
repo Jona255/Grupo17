5872876b388a8f1ae596d6dfa6053474292d326d
User <User@DESKTOP-85PBKTA>
2022-04-19 15:07:34 -0300

    Agregue excepción al ejercicio 3

--- TP2_Grupo17/src/main/java/ar/edu/unju/fi/ejercicio3/NumeroPar.java
+++ TP2_Grupo17/src/main/java/ar/edu/unju/fi/ejercicio3/NumeroPar.java
@@ -1,24 +1,36 @@
 package ar.edu.unju.fi.ejercicio3;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class NumeroPar {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		boolean continua;
+		int numero;
 		for (int i = 1; i <= 5; i++) {
+			do {
+				try {
+					continua = false;
+					System.out.print("Ingrese número "+i+": ");
+					numero = sc.nextInt();
+					if ((numero % 2) == 0) {
+						System.out.println("El número ingresado es par.");
+					} else {
+						System.out.println("El número no es par.");
+					}
+				} catch (InputMismatchException ex) {
+					System.out.println("Debe ingresar un número entero.");
+					sc.nextLine();
+					continua = true;
+				}
 
-			System.out.println("Ingrese un número: ");
-
-			Scanner sc = new Scanner(System.in);
-			int numero = sc.nextInt();
-
-			if ((numero % 2) == 0) {
-				System.out.println("El número ingresado es par.");
-			} else {
-				System.out.println("El número no es par.");
-			}
+			} while (continua);
 
 		}
+		sc.close();
+		System.out.println("*************************************");
 	}
 }
